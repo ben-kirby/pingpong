dbebf82ad2b06efb7464edc4d4f7928d1d14eb67
Ben Kirby <benkirby@Bens-MacBook-Pro.local>
2018-06-25 19:46:08 -0700

    It will now properly hide and display the list. Still working on a reset option

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
 var numbers = [];
+var test;
 //----------------------------
 $(document).ready(function(){
 
   $(".number").submit(function(){
     var i;
-    var input = $("#numberInput").val();
-    input = parseInt(input);
+    var input = parseInt($("#numberInput").val());
 
-    for (input; input > 0; input--) {
-      numbers.push(input);
+    for (var i = 0; i < input; i++) {
+      numbers.push(i+1);
     }
+
+    for (var i = 0; i < numbers.length; i++) {
+      test = numbers[i];
+
+      if ((test % 3 === 0) && (test % 5 ===0)) {
+        $("#list").append($("<li>").text("pingpong"));      }
+      else if (test % 3 === 0) {
+        $("#list").append($("<li>").text("ping"));
+      }
+      else if (test % 5 === 0) {
+        $("#list").append($("<li>").text("pong"));
+      }
+      else {
+        $("#list").append($("<li>").text(test));
+      }
+
+    }
+
     event.preventDefault();
   });
 });
