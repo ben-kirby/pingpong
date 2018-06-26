4299ea73de23ce3cc538cb36468305c4680a3a6c
Ben Kirby <benkirby@Bens-MacBook-Pro.local>
2018-06-25 19:54:28 -0700

    It will now have the list making function in the UI logic portion. No funtionality changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 var numbers = [];
 var test;
+var makeList = function(){
+  for (var i = 0; i < numbers.length; i++) {
+    test = numbers[i];
+
+    if ((test % 3 === 0) && (test % 5 ===0)) {
+      $("#list").append($("<li>").text("pingpong"));
+    }
+    else if (test % 3 === 0) {
+      $("#list").append($("<li>").text("ping"));
+    }
+    else if (test % 5 === 0) {
+      $("#list").append($("<li>").text("pong"));
+    }
+    else {
+      $("#list").append($("<li>").text(test));
+    }
+  }
+}
 //----------------------------
 $(document).ready(function(){
 
@@ -10,24 +28,7 @@ $(document).ready(function(){
     for (var i = 0; i < input; i++) {
       numbers.push(i+1);
     }
-
-    for (var i = 0; i < numbers.length; i++) {
-      test = numbers[i];
-
-      if ((test % 3 === 0) && (test % 5 ===0)) {
-        $("#list").append($("<li>").text("pingpong"));      }
-      else if (test % 3 === 0) {
-        $("#list").append($("<li>").text("ping"));
-      }
-      else if (test % 5 === 0) {
-        $("#list").append($("<li>").text("pong"));
-      }
-      else {
-        $("#list").append($("<li>").text(test));
-      }
-
-    }
-
+    makeList();
     event.preventDefault();
   });
 });
