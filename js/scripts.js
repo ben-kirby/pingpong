9e51fa994ab0dd6e835dc88fb89ec5404c119734
Ben Kirby <benkirby@Bens-MacBook-Pro.local>
2018-07-08 11:24:55 -0700

    It will now have further separated business and UI logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,6 @@
 //business logic
 var numbers = [];
-
-var makeList = function(){
-  for (var i = 0; i < numbers.length; i++) {
-
-    if ((numbers[i] % 3 === 0) && (numbers[i] % 5 ===0)) {
-      $("#list").append($("<li>").text("pingpong"));
-    }
-    else if (numbers[i] % 3 === 0) {
-      $("#list").append($("<li>").text("ping"));
-    }
-    else if (numbers[i] % 5 === 0) {
-      $("#list").append($("<li>").text("pong"));
-    }
-    else {
-      $("#list").append($("<li>").text(numbers[i]));
-    }
-  }
-}
+var results = []
 
 var outputList = function(){
     var numberInput = parseInt($("#numberInput").val());
@@ -25,25 +8,48 @@ var outputList = function(){
     for (var i = 0; i < numberInput; i++) {
       numbers.push(i+1);
     }
-    makeList();
+    counter();
     $(".list").toggle();
     $("#reset").show();
-}
+};
 
-var reset = function(){
-  $(".list").hide();
+var counter = function(){
+  for (var i = 0; i < numbers.length; i++){
+    if (numbers[i] % 15 === 0){
+      results.push("Pingpong!");
+    }
+    else if (numbers[i] % 5 === 0) {
+      results.push("Pong!");
+    }
+    else if (numbers[i] % 3 === 0) {
+      results.push("Ping!");
+    }
+    else {
+      results.push(i+1);
+    }
+  }
+};
+
+  var reset = function(){
   numbers = [];
+  results = [];
   $("#list").empty();
-  $("#reset").hide();
 }
 //UI Logic-----------------------------------
 $(document).ready(function(){
   $(".number").submit(function(){
     outputList();
+    for (var i = 0; i < results.length; i++) {
+      $("#list").append($("<li>").text(results[i]));
+    }
+    $("#submit").hide();
     event.preventDefault();
   });
 
   $("#reset").click(function(){
     reset();
+    $(".list").hide();
+    $("#reset").hide();
+    $("#submit").show();
   });
 });
